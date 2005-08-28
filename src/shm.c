c461dd8c6efa70fb3c20037d2bed434846562909
Helmut Januschka <helmut@januschka.com>
2005-08-28 16:02:59 +0000

    CVS Header

--- src/shm.c
+++ src/shm.c
@@ -1,3 +1,27 @@
+/* $Id$ */
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2005 Helmut Januschka - All Rights Reserved
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, Inc., 675 Mass Ave, Cambridge MA 02139,
+ *   USA; either version 2 of the License, or (at your option) any later
+ *   version; incorporated herein by reference.
+ *
+ * ----------------------------------------------------------------------- */
+/*
+$Revision$
+$Source$
+
+
+$Log$
+Revision 1.2  2005/08/28 16:02:59  hjanuschka
+CVS Header
+
+
+*/
+
 #include <string.h>
 #include <stdlib.h>
 
