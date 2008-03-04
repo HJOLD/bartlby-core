dc3db08cf3feef73ca196e44c67aca3c6041bba4
Helmut Januschka <helmut@januschka.com>
2008-03-04 11:41:21 +0000

    auto commit

--- include/bartlby.h
+++ include/bartlby.h
@@ -1,8 +1,8 @@
 
 #define PROGNAME "bartlby"
-#define REL_NAME "zero-eight"
-#define VERSION  "1.3.0"
-#define EXPECTCORE 1103051 //Module V Check's
+#define REL_NAME "autobot"
+#define VERSION  "1.3.1"
+#define EXPECTCORE 1103151 //Module V Check's
 
 #define MAX_CCACHE 1024
 
